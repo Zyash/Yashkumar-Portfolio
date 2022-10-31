f0736ba1ef28062aa7bf8e5b811a4a73612b2cfa
Zyash <yashkumar.patel@ucalgary.ca>
2022-10-31 14:22:35 -0600

    Project

---
+++ src/theme/themes.js
@@ -0,0 +1,45 @@
+export const lightTheme = {
+  background: '#fff',
+  color: '#121212',
+  accentColor: '#3D84C6',
+  chronoTheme: {
+    cardBgColor: 'pink',
+    cardForeColor: 'Darkgreen',
+    titleColor: 'blue',
+  },
+  timelineLineColor: '#3390FF',
+  cardBackground: 'green',
+  cardFooterBackground: 'green',
+  cardBorderColor: '#00000020',
+  navbarTheme: {
+    linkColor: '#33FF57',
+    linkHoverColor: '#DBFF33',
+    linkActiveColor: '#fefefe',
+  },
+  bsPrimaryVariant: 'light',
+  bsSecondaryVariant: 'dark',
+  socialIconBgColor: 'pink',
+};
+
+export const darkTheme = {
+  background: '#121212',
+  color: 'white',
+  accentColor: '#3D84C6',
+  chronoTheme: {
+    cardBgColor: '#1B1B1B',
+    cardForeColor: '#eee',
+    titleColor: 'pink',
+  },
+  timelineLineColor: '#444',
+  cardBackground: 'white',
+  cardFooterBackground: '#181818',
+  cardBorderColor: '#ffffff20',
+  navbarTheme: {
+    linkColor: '#dedede',
+    linkHoverColor: '#DBFF33',
+    linkActiveColor: '#DBFF33',
+  },
+  bsPrimaryVariant: 'dark',
+  bsSecondaryVariant: 'light',
+  socialIconBgColor: '#fefefe',
+};
